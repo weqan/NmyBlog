ca5a7edb3911a57970b81ee39eb449b47a615b5a
weqan <weqanhugo@gmail.com>
2019-03-12 13:31:17 +0800

    2019年3月12日 标签管理完成

--- NmyBlog.Web/Areas/Adnn1n/Controllers/CategoryController.cs
+++ NmyBlog.Web/Areas/Adnn1n/Controllers/CategoryController.cs
@@ -8,11 +8,11 @@ using NmyBlog.Model;
 
 namespace NmyBlog.Web.Areas.Adnn1n.Controllers
 {
+    [Area("Adnn1n")]
     public class CategoryController : Controller
     {
         CategoryDAL cadal = new CategoryDAL();
 
-        [Area("Adnn1n")]
         public IActionResult Index()
         {
 
@@ -20,44 +20,38 @@ namespace NmyBlog.Web.Areas.Adnn1n.Controllers
             return View(list);
         }
 
-        //public IActionResult Add(int? id)
-        //{
-        //    Category ca = new Category();
-        //    if (id != null)
-        //    {
-        //        ca = cadal.GetModel(id.Value);
-        //    }
+        public IActionResult Add(int? id)
+        {
+            Category ca = new Category();
+            if (id != null)
+            {
+                ca = cadal.GetModel(id.Value);
+            }
 
-        //    return View(ca);
-        //}
+            return View(ca);
+        }
 
-        //[HttpPost]
-        //public IActionResult Add(Blog bo)
-        //{
-        //    Category ca = cadal.GetModelByBh(bo.CaBh);
-        //    if (ca != null)
-        //    {
-        //        bo.CaName = ca.CaName;
-        //    }
+        [HttpPost]
+        public IActionResult Add(Category ca)
+        {
+            if (ca.Id == 0)
+            {
+                //新增
+                cadal.Insert(ca);
+            }
+            else
+            {
+                //修改
+                cadal.Update(ca);
+            }
 
-        //    if (bo.Id == 0)
-        //    {
-        //        //新增
-        //        bodal.Insert(bo);
-        //    }
-        //    else
-        //    {
-        //        //修改
-        //        bodal.Update(bo);
-        //    }
+            return Redirect("/Adnn1n/Category");
+        }
 
-        //    return Redirect("/Adnn1n/Blog/Index");
-        //}
-
-        //public IActionResult Del(int id)
-        //{
-        //    bodal.Delete(id);
-        //    return Redirect("/Adnn1n/Blog/Index");
-        //}
+        public IActionResult Del(int id)
+        {
+            cadal.Delete(id);
+            return Redirect("/Adnn1n/Category");
+        }
     }
 }
